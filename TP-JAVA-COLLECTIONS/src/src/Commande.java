7136a45fd7f355578a74054c2e8cdb5c5883aad4
elhajaouioumaima882-a11y <elhajaouioumaima882@gmail.com>
2025-10-27 10:39:12 +0100

    Update Commande.java

--- TP-JAVA-COLLECTIONS/src/src/Commande.java
+++ TP-JAVA-COLLECTIONS/src/src/Commande.java
@@ -1,5 +1,65 @@
-package src;
+import java.util.Date;
 
-public class Commande {
+public class Commande implements Comparable<Commande> {
+    private int numCommande;
+    private Date dateCommande;
+    private String nomFournisseur;
 
+    // Constructeur avec arguments
+    public Commande(int numCommande, Date dateCommande, String nomFournisseur) {
+        this.numCommande = numCommande;
+        this.dateCommande = dateCommande;
+        this.nomFournisseur = nomFournisseur;
+    }
+
+    // Getters
+    public int getNumCommande() {
+        return numCommande;
+    }
+
+    public Date getDateCommande() {
+        return dateCommande;
+    }
+
+    public String getNomFournisseur() {
+        return nomFournisseur;
+    }
+
+    // Setters
+    public void setNumCommande(int numCommande) {
+        this.numCommande = numCommande;
+    }
+
+    public void setDateCommande(Date dateCommande) {
+        this.dateCommande = dateCommande;
+    }
+
+    public void setNomFournisseur(String nomFournisseur) {
+        this.nomFournisseur = nomFournisseur;
+    }
+
+    // Méthode toString
+    @Override
+    public String toString() {
+        return "Commande{" +
+                "numCommande=" + numCommande +
+                ", dateCommande=" + dateCommande +
+                ", nomFournisseur='" + nomFournisseur + '\'' +
+                '}';
+    }
+
+    // Méthode equals
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Commande commande = (Commande) obj;
+        return numCommande == commande.numCommande;
+    }
+
+    // Implémentation de Comparable pour trier par date
+    @Override
+    public int compareTo(Commande autre) {
+        return this.dateCommande.compareTo(autre.dateCommande);
+    }
 }
