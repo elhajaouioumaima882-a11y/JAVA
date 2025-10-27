3ccbb0a6fc8f19b0fb2e2d68b9983d5544eecd0d
elhajaouioumaima882-a11y <elhajaouioumaima882@gmail.com>
2025-10-27 10:38:53 +0100

    Update ClientFidel.java

--- TP-JAVA-COLLECTIONS/src/src/ClientFidel.java
+++ TP-JAVA-COLLECTIONS/src/src/ClientFidel.java
@@ -1,5 +1,43 @@
-package src;
+public class ClientFidel extends Client {
+    private String codeFidelite;
+    private float tauxReduction;
 
-public class ClientFidel {
+    // Constructeur d'initialisation
+    public ClientFidel(int codeClient, String nomClient, String adrClient, 
+                      String codeFidelite, float tauxReduction) {
+        super(codeClient, nomClient, adrClient);
+        this.codeFidelite = codeFidelite;
+        this.tauxReduction = tauxReduction;
+    }
 
+    // Getters et Setters
+    public String getCodeFidelite() {
+        return codeFidelite;
+    }
+
+    public void setCodeFidelite(String codeFidelite) {
+        this.codeFidelite = codeFidelite;
+    }
+
+    public float getTauxReduction() {
+        return tauxReduction;
+    }
+
+    public void setTauxReduction(float tauxReduction) {
+        this.tauxReduction = tauxReduction;
+    }
+
+    // Méthode toString redéfinie
+    @Override
+    public String toString() {
+        return "ClientFidel{" +
+                "codeClient=" + getCodeClient() +
+                ", nomClient='" + getNomClient() + '\'' +
+                ", adrClient='" + getAdrClient() + '\'' +
+                ", telClient='" + getTelClient() + '\'' +
+                ", codeFidelite='" + codeFidelite + '\'' +
+                ", tauxReduction=" + tauxReduction +
+                ", commandes=" + getListCommandes() +
+                '}';
+    }
 }
